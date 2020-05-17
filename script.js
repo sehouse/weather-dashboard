a43f7cbc33f84cb96de7c151d132cd37f8a34c41
sehouse <sehouse@umich.edu>
2020-05-17 14:27:45 -0400

    Creation of function to call for UV index and append it to the html. Latitude and longitude took time to get working.

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 var city;
 var queryUrl;
+var latitude;
+var longitude;
 
 // Click event to pull search data from input field
 
@@ -22,6 +24,8 @@ function apiCall(queryUrl) {
         url: queryUrl,
         method: "GET",
         success: function (response) {
+
+            console.log(response);
             city = response.name;
             $("#city").text(response.name);
             $("#date").text(" (" + moment().format('l') + ")");
@@ -30,7 +34,40 @@ function apiCall(queryUrl) {
             $("#currentTemp").html("Temperature: " + temp);
             $("#currentHumidity").text("Humidity: " + response.main.humidity + "%");
             $("#currentWindSpeed").text("Wind Speed: " + response.wind.speed + "MPH");
+            latitude = response.coord.lat;
+            longitude = response.coord.lon;
+            uvIndex();
         }
 
     })
+}
+
+//Function to send a request to OpenWeather API for the UV Index values for the searched city, as it uses a different API call to get that data.
+
+function uvIndex() {
+    var queryUrl = "https://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&units=imperial&APPID=bfc1b977d5f0ad912b3dc6c21e34e887";
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function (response) {
+        var uv = response.value;
+        uvText = "white";
+        if (uv < 3) {
+            uvColor = "green";
+        }
+        else if (uv >= 3 && uv < 6) {
+            uvColor = "yellow";
+        }
+        else if (uv >= 6 && uv < 8) {
+            uvColor = "orange";
+        }
+        else if (uv >= 8 && uv < 11) {
+            uvColor = "red";
+        }
+        else {
+            uvColor = "purple";
+        }
+        $('#currentUV').html("UV Index: <span>" + uv + "</span>");
+        $('#currentUV span').css({ "background-color": uvColor, "color": uvText, "border-radius": "5px" }).addClass("px-2");
+    });
 }
